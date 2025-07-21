a054a46a8d1db902f10af2d97a3cfd66582211f3
Maxime R. <cocatrix@laposte.net>
2025-07-21 21:09:38 +0200

    Create Game and Box classes and global variables

---
+++ game.py
@@ -0,0 +1,72 @@
+from abc import ABC, abstractmethod
+from dataclasses import dataclass, field
+from typing import List
+
+class CoverImageMixin(ABC):
+    @property
+    def cover_image_path(self) -> str:
+        return f"/static/images/{self.image_name}.webp"
+
+    @property
+    @abstractmethod
+    def image_name(self) -> str:
+        ...
+
+@dataclass(frozen=True)
+class Game(CoverImageMixin):
+    key: str
+    name: str
+
+    @property
+    def image_name(self) -> str:
+        return f"{self.key}_core"
+
+@dataclass(frozen=True)
+class Box(CoverImageMixin):
+    box_suffix: str
+    game: Game
+    box_name: str
+    key: str = field(init=False)
+
+    def __post_init__(self):
+        object.__setattr__(self, "key", f"{self.game.key}_{self.box_suffix}")
+
+    @property
+    def image_name(self) -> str:
+        return self.key
+
+    @property
+    def full_name(self) -> str:
+        return f"{self.game.name} {self.box_name}"
+
+
+@dataclass(frozen=True)
+class OriginalBox(Box):
+    def __init__(self, game: Game):
+        super().__init__(box_suffix="core", game=game, box_name=game.name)
+
+    @property
+    def full_name(self) -> str:
+        return self.box_name
+
+# --- Global variables
+
+AEONSEND = Game("aeonsend", "Aeon's End")
+CHALLENGERS = Game("challengers", "Challengers!")
+LEGENDARY = Game("legendary", "Legendary")
+ALL_GAMES: List[Game] = [
+    AEONSEND,
+    CHALLENGERS,
+    LEGENDARY,
+]
+
+AEONSEND_BOXES: List[Box] = [
+    OriginalBox(AEONSEND),
+    Box("parias", AEONSEND, "Les Parias"),
+]
+
+CHALLENGERS_BOXES: List[Box] = [
+    OriginalBox(CHALLENGERS),
+    Box("beach", CHALLENGERS, "Beach Cup"),
+    Box("rumble", CHALLENGERS, "Rumble"),
+]
