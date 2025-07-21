12c21a020ad073817202ea6ea973a9b377e44540
Maxime R <cocatrix@laposte.net>
2025-07-21 20:21:49 +0200

    Create page renderer (HTML basics + scripts and css loading)

---
+++ render_page.py
@@ -0,0 +1,36 @@
+from textwrap import dedent
+
+from strings import (
+    SITE_DESC,
+)
+
+def _render_page(body_string: str, title: str, key: str) -> str:
+    """
+    Builds full HTML page, injecting `body_string`
+    and loading JS/CSS files named `key`.
+    """
+    icon_path = '/static/images/favicon.ico'
+
+    html_head = f'''
+    <!DOCTYPE html>
+    <html lang="fr">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="description" content="{SITE_DESC}">
+        <link rel="icon" type="image/x-icon" href="{icon_path}">
+        <title>{title}</title>
+        <link rel="stylesheet" href="/static/css/base.css">
+        <link rel="stylesheet" href="/static/css/{key}.css">
+    </head>
+    <body>
+    '''
+    
+    html_foot = f'''
+        <script src="/static/scripts/utils.js"></script>
+        <script src="/static/scripts/{key}.js"></script>
+    </body>
+    </html>
+    '''
+
+    return dedent(html_head + body_string + html_foot).strip()
---
+++ strings.py
@@ -0,0 +1,7 @@
+SITE_DESC = (
+    "Setup Express est un randomizer fait maison :"
+    " il choisit des sets/familles/cartes aléatoires, parmi les choix"
+    " disponibles selon les boîtes choisies (box core, extension, standalone)."
+    " Par cocatrix@laposte.net, hébergé gratuitement sur PythonAnywhere"
+)
+SITE_NAME = "Setup Express"
